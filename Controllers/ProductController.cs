9a55374580be91d90490db58ea871c777b6e34ea
Irfan Mohammed <irfanmohammed09@outlook.com>
2021-09-01 10:30:59 +0530

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using CrudOps.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudOps.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        ProductEntities dbobj = new ProductEntities();
+        public ActionResult Product()
+        {
+            return View();
+        }
+        public ActionResult GetProduct()
+        {
+            var list = dbobj.Products.ToList();
+            return View(list);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CrudOps.MvcApplication" Language="C#" %>
